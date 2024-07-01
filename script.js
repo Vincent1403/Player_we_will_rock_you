dade406b2534945b893f0d43955b154d67b0bb61
Vincent1403 <115629387+Vincent1403@users.noreply.github.com>
2024-07-01 18:56:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var audio = new Audio("https://vincent1403.github.io/Sounds/fall_in_love.mp3");
+var audio = new Audio("https://vincent1403.github.io/Sounds/we_will_rock_you.mp3");
 
 $('#play-pause-button').on("click",function(){
   if($(this).hasClass('fa-play'))
